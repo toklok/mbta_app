3dc0f37c6a0b841646ced2b6e5994bad4e15370a
Joseph Curtis <joecurt85@gmail.com>
2017-01-11 14:30:27 -0500

    add loaders to go through transformations, update app.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
 module.exports = {
-  entry: "./web/static/js/app.js",
+  entry: './web/static/js/app.js',
   output: {
-    path: "./priv/static/js",
-    filename: "app.js"
+    path: './priv/static/js',
+    filename: 'app.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }]
+  },
+  resolve: {
+    modulesDirectories: [ 'node_modules', __dirname + '/web/static/js' ]
   }
-};
+}
