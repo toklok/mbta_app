b4dfcda41a13b648e9356265e020c6a2c3278102
Joseph Curtis <joecurt85@gmail.com>
2017-01-11 16:29:23 -0500

    update package, and update for auto reload

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -4,6 +4,8 @@ import { Socket } from "phoenix";
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+require('/home/jcurtis/mbta_app/web/static/css/app.scss')
+
 ReactDOM.render(
     <h1>HELLO WORLD!</h1>,
     document.getElementById('root')
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
+  devtool: 'eval-source-map',
+  debug: true,
   entry: './web/static/js/app.js',
   output: {
     path: './priv/static/js',
     filename: 'app.js'
   },
+  plugins: [
+    new ExtractTextPlugin('../css/app.css')
+  ],
   module: {
     loaders: [
       {
         test: /\.js?$/,
         exclude: /node_modules/,
         loader: 'babel'
-      }]
-  },
-  resolve: {
-    modulesDirectories: [ 'node_modules', __dirname + '/web/static/js' ]
-  }
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style','css?sourceMap!sass?sourceMap')
+      }
+  ]
+}
 }
