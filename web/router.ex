8e10ec663d6529fb07ee9c5a146ea0d0d560e399
Joseph Curtis <joecurt85@gmail.com>
2017-01-11 16:00:00 -0500

    change template, render hello world

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule MbtaApp.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+
   end
 
   # Other scopes may use custom stacks.
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,3 +1,10 @@
 // Import dependencies
 import "phoenix_html";
 import { Socket } from "phoenix";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>HELLO WORLD!</h1>,
+    document.getElementById('root')
+);
